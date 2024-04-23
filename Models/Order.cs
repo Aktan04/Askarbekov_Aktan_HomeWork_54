4974fafb9fbd373d6fd37efda1da22a922e3b847
Askarbekov Aktan <akn392204@gmail.com>
2024-04-23 17:41:13 +0600

    Add model of Order and download nuget packages

---
+++ Models/Order.cs
@@ -0,0 +1,12 @@
+namespace SecondProductShop.Models;
+
+public class Order
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public string ContactPhone { get; set; }
+    
+    public int ProductId { get; set; }
+    public Product? Product { get; set; }
+}
