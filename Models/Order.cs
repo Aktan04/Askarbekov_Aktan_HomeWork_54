7634bd8008324fa0423629d3eb71892691caba52
Askarbekov Aktan <akn392204@gmail.com>
2024-04-23 18:13:18 +0600

    Add validation of Order and Index, Create Views of OrderController

--- Models/Order.cs
+++ Models/Order.cs
@@ -1,10 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace SecondProductShop.Models;
 
 public class Order
 {
     public int Id { get; set; }
+    [Required(ErrorMessage = "Поле 'Название' обязательно для заполнения")]
     public string Name { get; set; }
+    [Required(ErrorMessage = "Поле 'Адресс' обязательно для заполнения")]
     public string Address { get; set; }
+    [Required(ErrorMessage = "Поле 'Контактный телефон' обязательно для заполнения")]
     public string ContactPhone { get; set; }
     
     public int ProductId { get; set; }
